4c1fc7dfb9e2c2fc0e435e1b64e2c2f04b5b0a1c
Robertas Vilkas <rob.vilkas@gmail.com>
2023-04-02 20:52:37 +0300

    going for account creation

---
+++ Exam/DAL/IUserInformationDbRepository.cs
@@ -0,0 +1,19 @@
+﻿using Exam.DTO;
+
+namespace Exam.DAL
+{
+    public interface IUserInformationDbRepository
+    {
+        void AddNewUserToList(int id, UserInformationDto UserDto);
+        void UpdateUserById(int id, string name, string surname,
+            string personalCode, string phone, string email,
+            string city, string street, string houseNumber, string flatNumber);
+        public List<UserInformationDto> GetAllAccounts();
+        public void DeleteUserById(int id);
+
+        public IEnumerable<UserDto> GetAllUserInfo();
+
+        public UserDto GetUserInformationById(int userId);
+
+    }
+}
