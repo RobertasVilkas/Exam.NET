f4d55241667194a140da9dfb1036f8a0abce9a9f
Robertas Vilkas <rob.vilkas@gmail.com>
2023-04-02 19:41:43 +0300

    creating user registration dbcontext

---
+++ Exam/UserAccount.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeAcademyFinalExam
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+        public string Role { get; set; }
+
+        public int? HumanInformationId { get; set; }
+
+        [ForeignKey("HumanInformationId")]
+        public HumanInformation? HumanInformation { get; set; }
+    }
+}
---
+++ Exam/UserRegistrationDbContextcs.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace CodeAcademyFinalExam
+{
+    public class UserRegistrationDbContext : DbContext
+    {
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+        public DbSet<Address> Address { get; set; }
+
+        public DbSet<HumanInformation> HumanInformation { get; set; }
+
+        public UserRegistrationDbContext(DbContextOptions<UserRegistrationDbContext> options) : base(options)
+        {
+
+        }
+        public bool DoesUserExist(string username)
+        {
+            return Accounts.Any(a => a.Username == username);
+        }
+    }
+}
