f9e22dd5269c32ae5f343234bf6190765439a12d
Robertas Vilkas <rob.vilkas@gmail.com>
2023-04-02 19:43:08 +0300

    creating user adress

---
+++ Exam/UserAdress.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exam
+{
+    public class UserAddress
+    {
+        [Key]
+        public int AddressId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string? FlatNumber { get; set; }
+    }
+}
