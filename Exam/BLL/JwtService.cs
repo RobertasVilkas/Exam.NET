57fdaf26d419604b8e8f90112b2808a4fa1a39b9
Robertas Vilkas <rob.vilkas@gmail.com>
2023-04-02 19:52:45 +0300

    creating jwt service

---
+++ Exam/BLL/JwtService.cs
@@ -0,0 +1,39 @@
+﻿using Exam.BLL;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Exam.BLL
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetJwtToken(string username, int accountId, string role)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.NameIdentifier, accountId.ToString()),
+                new Claim(ClaimTypes.Role, role)
+            };
+            var secretToken = _configuration.GetSection("Jwt:Key").Value;
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretToken));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration.GetSection("Jwt:Issuer").Value,
+                audience: _configuration.GetSection("Jwt:Audience").Value,
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
