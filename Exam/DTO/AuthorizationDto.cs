3f54a0c8bd90df219cdc11eb8d04e039c41e0c15
Robertas Vilkas <rob.vilkas@gmail.com>
2023-04-02 21:10:54 +0300

    creating authorization and personal info for data transfer

---
+++ Exam/DTO/AuthorizationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exam.DTO
+{
+    public class AuthorizationDto
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Password { get; set; }
+    }
+}
---
+++ Exam/DTO/PersonalInformationDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exam.DTO
+{
+    public class PersonalInformationDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        [StringLength(11, MinimumLength = 11)]
+        public string PersonalCode { get; set; }
+        [Required]
+        [Phone]
+        public string TelephoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public UserAddressDto Address { get; set; }
+    }
+}
