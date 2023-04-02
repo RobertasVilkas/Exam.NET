fd50e766f71f2a71e46bcfddb74c6ac2f59eade7
Robertas Vilkas <rob.vilkas@gmail.com>
2023-04-02 21:05:49 +0300

    creating adress base for data transfer

---
+++ Exam/DTO/UserAdressDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exam.DTO
+{
+    public class UserAddressDto
+    {
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string HouseNumber { get; set; }
+        [Required]
+        public string FlatNumber { get; set; }
+    }
+}
