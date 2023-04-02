7a7cac68a7cefffee2d5097c009204e6c37e52c4
Robertas Vilkas <rob.vilkas@gmail.com>
2023-04-02 20:55:47 +0300

    jwt repository creation

---
+++ Exam/DAL/JwtRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Exam.DAL
+{
+    public class JwtRepository : IJwtRepository
+    {
+        readonly UserRegistrationDbContext _context;
+
+        public JwtRepository(UserRegistrationDbContext context)
+        {
+            _context = context;
+        }
+
+        public UserAccount GetAccount(string username)
+        {
+            return _context.UserAccounts.FirstOrDefault(A => A.Username == username);
+        }
+
+        public void SaveAccount(UserAccount account)
+        {
+            _context.UserAccounts.Add(account);
+            _context.SaveChanges();
+        }
+    }
+}
